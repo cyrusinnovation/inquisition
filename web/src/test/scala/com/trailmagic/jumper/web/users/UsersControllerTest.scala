31ab5e96661dcf612b8f20e9ca91985b5ff20eb5
Nicholas Goodwin <ngoodwin@cyrusinnovation.com>
2011-10-03 16:02:15 -0400

    Writing tests to verify that the user object is added when there are errors present

--- web/src/test/scala/com/trailmagic/jumper/web/users/UsersControllerTest.scala
+++ web/src/test/scala/com/trailmagic/jumper/web/users/UsersControllerTest.scala
@@ -6,12 +6,18 @@ import org.junit.Assert._
 import org.junit.{Before, Test}
 import org.mockito.{MockitoAnnotations, Mock}
 import com.trailmagic.jumper.core.{InMemoryUserRepository, User, UserRepository}
+import org.springframework.validation.{ObjectError, BindingResult}
+import scala.collection.JavaConverters._;
+import org.mockito.Mockito.when
+
+
 
 
 class UsersControllerTest {
   var controller: UsersController = _
   var userRepository: UserRepository = _
   @Mock var userService: UserService = _
+  @Mock var bindingData: BindingResult = _
   val TestUser = User("testy@example.com", "tester", "Testy", "McTesterton", "password", "salt")
 
   @Before
@@ -19,6 +25,26 @@ class UsersControllerTest {
     MockitoAnnotations.initMocks(this)
     userRepository = new InMemoryUserRepository
     controller = new UsersController(userRepository, userService)
+  }
+
+
+
+  @Test
+  def testCreateNewUserUser_with_errors() {
+
+    val userFormData = new UserFormData();
+    userFormData.setEmail("a@example.com");
+    userFormData.setFirstName("Joe");
+
+                    val passwordError = new ObjectError("password", "password too short");
+
+    val errors = List(passwordError).asJava;
+
+    when(bindingData.getAllErrors).thenReturn(errors);
+
+    val mav = controller.createNewUser(userFormData, bindingData)
+    assertTrue(mav.getModel.get("user") != null)
+    assertEquals(userFormData, mav.getModel.get("user"))
   }
 
   @Test
@@ -34,4 +60,4 @@ class UsersControllerTest {
   def testUserProfilePageThrowsExceptionWhenUserNotFound() {
     controller.displayUserProfile("no user")
   }
-  }
+}
