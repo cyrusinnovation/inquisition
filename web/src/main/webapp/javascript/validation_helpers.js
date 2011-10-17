6b3d80d04c8664f6746b63f242dfc7c84edce048
Nicholas Goodwin <ngoodwin@cyrusinnovation.com>
2011-10-17 14:12:32 -0400

    * Added html changes for javascript tag autocompletion * Added javascript for tag autocompletion * Added css for tag autocompletion

--- web/src/main/webapp/javascript/validation_helpers.js
+++ web/src/main/webapp/javascript/validation_helpers.js
@@ -10,57 +10,71 @@ var validateEmail = function(email, error) {
 };
 
 var validatePassword = function(password, error) {
-	if(password.value.length < 8)
-	{
-		error.innerHTML = 'Password must be at least eight characters!';
-	}
-	else
-	{
-		error.innerHTML = '';
-	}
+    if (password.value.length < 8) {
+        error.innerHTML = 'Password must be at least eight characters!';
+    }
+    else {
+        error.innerHTML = '';
+    }
 };
 
 var setFocus = function(elementid) {
-  setTimeout("document.getElementById('" + elementid + "').focus();", 100);
+    setTimeout("document.getElementById('" + elementid + "').focus();", 100);
 };
 
 var dialogFunction = function() {
-  $("#dialog").dialog({
-    autoOpen: false,
-    modal: true
-  })};
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true
+    })
+};
 
 $(document).ready(dialogFunction);
 
 $(".confirmLink").click(function(e) {
-  e.preventDefault();
-  var targetUrl = $(this).attr("value");
+    e.preventDefault();
+    var targetUrl = $(this).attr("value");
 
-  $("#dialog").dialog({
-    buttons : {
-      "Confirm" : function() {
-          var f = document.createElement('form');
-          $(this).after($(f).attr({
-          method: 'POST',
-          action: $(this).attr('value')
-      }).append('<input type="hidden" name="_method" value="DELETE" />'));
-      $(f).submit();
+    $("#dialog").dialog({
+        buttons : {
+            "Confirm" : function() {
+                var f = document.createElement('form');
+                $(this).after($(f).attr({
+                    method: 'POST',
+                    action: $(this).attr('value')
+                }).append('<input type="hidden" name="_method" value="DELETE" />'));
+                $(f).submit();
 //        window.location.href = targetUrl;
-      },
-      "Cancel" : function() {
-        $(this).dialog("close");
-      }
-    }
-  });
+            },
+            "Cancel" : function() {
+                $(this).dialog("close");
+            }
+        }
+    });
 
-  $("#dialog").dialog("open");
+    $("#dialog").dialog("open");
 });
 
-
 $("#questionDelete").click(function(event) {
-   event.preventDefault();
+    event.preventDefault();
 
-   // if the element is of type delete question, then
-   // override default action
-   // submit delete of /questions/${questionid}
+    // if the element is of type delete question, then
+    // override default action
+    // submit delete of /questions/${questionid}
+});
+
+$(document).ready(function() {
+    $("#tags").tagit({ tagSource: function(search, showChoices) {
+        $.ajax({
+            'url' : "/questions/tags/" + search.term,
+            'dataType' : 'json',
+            'async' : false, // wait until this is ajax hit is complete before continue
+            'success' : function (m) {
+                showChoices(m.tags);
+            }
+        });
+    }
+    });
+//    tagSuggest({
+//    url: '/questions/tags/
 });
